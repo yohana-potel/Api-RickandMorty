3a58a13b826ac51814b695d39828ab8499737383
Yoha-Potel <cpoteljunot@hotmail.com>
2024-03-02 16:03:57 -0300

    deploy hecho

--- src/components/CharacterList.jsx
+++ src/components/CharacterList.jsx
@@ -6,9 +6,9 @@ function PagNva({page, setPage}){
   return(
     <header className="d-flex justify-content-between align-items-center">
       <p>Page {page} </p>
-      
+    
       <button className="btn btn-primary btn-sm" 
-        onClick = {() => setPage(page + 1)} >
+        onClick = {() => setPage(page + 1)} > 
         Page {page + 1 }
       </button>
     </header>
--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "https://yohana-potel.github.io/Api-RickandMorty/",
   plugins: [react()],
 })
